85dce44b0a3d9f8a95d99b6d99520d7bc4488365
tgmorton <thomasmorton88@gmail.com>
2023-11-08 14:58:02 -0800

    Wow took a while to commit

--- packages/plugin-image-keyboard-response-logger/src/index.ts
+++ packages/plugin-image-keyboard-response-logger/src/index.ts
@@ -67,105 +67,90 @@ const info = <const>{
      * If true, the image will be drawn onto a canvas element (prevents blank screen between consecutive images in some browsers).
      * If false, the image will be shown via an img element.
      */
-    render_on_canvas: {
-      type: ParameterType.BOOL,
-      pretty_name: "Render on canvas",
-      default: true,
+    // render_on_canvas: {
+    //   type: ParameterType.BOOL,
+    //   pretty_name: "Render on canvas",
+    //   default: true,
+    // },
+    visual_feedback: {
+      type: ParameterType.STRING,
+      pretty_name: 'Whether to display something on the screen',
+      options: ['no', 'word', 'whole', 'long'],
+      default: 'whole',
+      description: 'Determines the type of feedback displayed on the screen during typing'
     },
+    /* trigger list object of type list with strings */
+    trigger_list: {
+      type: ParameterType.COMPLEX,
+      pretty_name: 'Trigger list',
+      default: [],
+      description: 'List of triggers to be displayed on the screen during typing'
+    },
+    trigger_end: {
+      type: ParameterType.BOOL,
+      pretty_name: 'Trigger end',
+      default: false,
+      description: 'Whether to trigger the end of the trial'
+    },
+    space_allowed : {
+      type: ParameterType.BOOL,
+      pretty_name: 'Space allowed',
+      default: true,
+      description: 'Whether to allow space to be used for word separation'
+    },
+    correct_feedback: {
+      type: ParameterType.BOOL,
+      pretty_name: 'Correct feedback',
+      default: false,
+      description: 'Whether to give feedback on correct typing'
+    },
+    correction_delay: {
+      type: ParameterType.INT,
+      pretty_name: 'Correct delay',
+      default: 750,
+      description: 'How long to display correct feedback'
+    },
+    }
   },
 };
 
 type Info = typeof info;
 
 /**
- * **image-keyboard-response**
+ * **image-keyboard-response-logger**
  *
- * jsPsych plugin for displaying an image stimulus and getting a keyboard response
+ * jsPsych plugin for displaying an image stimulus and getting a keyboard response adapted from Josh de Leeuw's plugin
  *
- * @author Josh de Leeuw
- * @see {@link https://www.jspsych.org/plugins/jspsych-image-keyboard-response/ image-keyboard-response plugin documentation on jspsych.org}
+ * @author Thomas Morton
  */
+
 class ImageKeyboardResponsePlugin implements JsPsychPlugin<Info> {
   static info = info;
 
-  constructor(private jsPsych: JsPsych) {}
-
-  trial(display_element: HTMLElement, trial: TrialType<Info>) {
+  renderImageOnCanvas(trial, display_element, image_drawn = false) {
+    // first clear the display element
+    while (display_element.firstChild) {
+      display_element.removeChild(display_element.firstChild);
+    }
+  
+    // create canvas element and image
+    var canvas = document.createElement("canvas");
+    canvas.id = "jspsych-image-keyboard-response-stimulus";
+    canvas.style.margin = "0";
+    canvas.style.padding = "0";
+    var ctx = canvas.getContext("2d");
+    var img = new Image();
     var height, width;
-    if (trial.render_on_canvas) {
-      var image_drawn = false;
-      // first clear the display element (because the render_on_canvas method appends to display_element instead of overwriting it with .innerHTML)
-      if (display_element.hasChildNodes()) {
-        // can't loop through child list because the list will be modified by .removeChild()
-        while (display_element.firstChild) {
-          display_element.removeChild(display_element.firstChild);
-        }
-      }
-      // create canvas element and image
-      var canvas = document.createElement("canvas");
-      canvas.id = "jspsych-image-keyboard-response-stimulus";
-      canvas.style.margin = "0";
-      canvas.style.padding = "0";
-      var ctx = canvas.getContext("2d");
-      var img = new Image();
-      img.onload = () => {
-        // if image wasn't preloaded, then it will need to be drawn whenever it finishes loading
-        if (!image_drawn) {
-          getHeightWidth(); // only possible to get width/height after image loads
-          ctx.drawImage(img, 0, 0, width, height);
-        }
-      };
-      img.src = trial.stimulus;
-      // get/set image height and width - this can only be done after image loads because uses image's naturalWidth/naturalHeight properties
-      const getHeightWidth = () => {
-        if (trial.stimulus_height !== null) {
-          height = trial.stimulus_height;
-          if (trial.stimulus_width == null && trial.maintain_aspect_ratio) {
-            width = img.naturalWidth * (trial.stimulus_height / img.naturalHeight);
-          }
-        } else {
-          height = img.naturalHeight;
-        }
-        if (trial.stimulus_width !== null) {
-          width = trial.stimulus_width;
-          if (trial.stimulus_height == null && trial.maintain_aspect_ratio) {
-            height = img.naturalHeight * (trial.stimulus_width / img.naturalWidth);
-          }
-        } else if (!(trial.stimulus_height !== null && trial.maintain_aspect_ratio)) {
-          // if stimulus width is null, only use the image's natural width if the width value wasn't set
-          // in the if statement above, based on a specified height and maintain_aspect_ratio = true
-          width = img.naturalWidth;
-        }
-        canvas.height = height;
-        canvas.width = width;
-      };
-      getHeightWidth(); // call now, in case image loads immediately (is cached)
-      // add canvas and draw image
-      display_element.insertBefore(canvas, null);
-      if (img.complete && Number.isFinite(width) && Number.isFinite(height)) {
-        // if image has loaded and width/height have been set, then draw it now
-        // (don't rely on img onload function to draw image when image is in the cache, because that causes a delay in the image presentation)
+  
+    img.onload = () => {
+      if (!image_drawn) {
+        getHeightWidth();
         ctx.drawImage(img, 0, 0, width, height);
-        image_drawn = true;
       }
-      // add prompt if there is one
-      if (trial.prompt !== null) {
-        display_element.insertAdjacentHTML("beforeend", trial.prompt);
-      }
-    } else {
-      // display stimulus as an image element
-      var html = '<img src="' + trial.stimulus + '" id="jspsych-image-keyboard-response-stimulus">';
-      // add prompt
-      if (trial.prompt !== null) {
-        html += trial.prompt;
-      }
-      // update the page content
-      display_element.innerHTML = html;
-
-      // set image dimensions after image has loaded (so that we have access to naturalHeight/naturalWidth)
-      var img = display_element.querySelector(
-        "#jspsych-image-keyboard-response-stimulus"
-      ) as HTMLImageElement;
+    };
+    img.src = trial.stimulus;
+  
+    const getHeightWidth = () => {
       if (trial.stimulus_height !== null) {
         height = trial.stimulus_height;
         if (trial.stimulus_width == null && trial.maintain_aspect_ratio) {
@@ -184,15 +169,153 @@ class ImageKeyboardResponsePlugin implements JsPsychPlugin<Info> {
         // in the if statement above, based on a specified height and maintain_aspect_ratio = true
         width = img.naturalWidth;
       }
-      img.style.height = height.toString() + "px";
-      img.style.width = width.toString() + "px";
+      canvas.height = height;
+      canvas.width = width;
+    };
+  
+    getHeightWidth();
+  
+    display_element.insertBefore(canvas, null);
+    if (img.complete && Number.isFinite(width) && Number.isFinite(height)) {
+      ctx.drawImage(img, 0, 0, width, height);
+      image_drawn = true;
+    }
+  
+    if (trial.prompt !== null) {
+      display_element.insertAdjacentHTML("beforeend", trial.prompt);
     }
 
-    // store response
-    var response = {
-      rt: null,
-      key: null,
+    return canvas;
+  }
+
+  changeImageOnCanvas(trial, canvas: HTMLCanvasElement, newImageSrc: string) {
+    var ctx = canvas.getContext("2d");
+    var img = new Image();
+    var height, width;
+  
+    img.onload = () => {
+      // Clear the canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      // Draw the new image
+      ctx.drawImage(img, 0, 0, width, height);
     };
+    img.src = newImageSrc;
+  
+    const updateDimensions = () => {
+      // Update the canvas dimensions if necessary
+      // This assumes that you have access to the same variables as in the original function
+      // If not, you'll need to pass them as parameters
+      if (trial.stimulus_height !== null) {
+        height = trial.stimulus_height;
+        if (trial.stimulus_width == null && trial.maintain_aspect_ratio) {
+          width = img.naturalWidth * (trial.stimulus_height / img.naturalHeight);
+        }
+      } else {
+        height = img.naturalHeight;
+      }
+      if (trial.stimulus_width !== null) {
+        width = trial.stimulus_width;
+        if (trial.stimulus_height == null && trial.maintain_aspect_ratio) {
+          height = img.naturalHeight * (trial.stimulus_width / img.naturalWidth);
+        }
+      } else if (!(trial.stimulus_height !== null && trial.maintain_aspect_ratio)) {
+        width = img.naturalWidth;
+      }
+      canvas.height = height;
+      canvas.width = width;
+    };
+  
+    updateDimensions();
+  }
+
+  // renderImageElement(trial, display_element) {
+  //   // display stimulus as an image element
+  //   var html = '<img src="' + trial.stimulus + '" id="jspsych-image-keyboard-response-stimulus">';
+    
+  //   // add prompt
+  //   if (trial.prompt !== null) {
+  //     html += trial.prompt;
+  //   }
+    
+  //   // update the page content
+  //   display_element.innerHTML = html;
+  
+  //   // set image dimensions after image has loaded (so that we have access to naturalHeight/naturalWidth)
+  //   var img = display_element.querySelector(
+  //     "#jspsych-image-keyboard-response-stimulus"
+  //   ) as HTMLImageElement;
+  //   var height, width;
+    
+  //   if (trial.stimulus_height !== null) {
+  //     height = trial.stimulus_height;
+  //     if (trial.stimulus_width == null && trial.maintain_aspect_ratio) {
+  //       width = img.naturalWidth * (trial.stimulus_height / img.naturalHeight);
+  //     }
+  //   } else {
+  //     height = img.naturalHeight;
+  //   }
+    
+  //   if (trial.stimulus_width !== null) {
+  //     width = trial.stimulus_width;
+  //     if (trial.stimulus_height == null && trial.maintain_aspect_ratio) {
+  //       height = img.naturalHeight * (trial.stimulus_width / img.naturalWidth);
+  //     }
+  //   } else if (!(trial.stimulus_height !== null && trial.maintain_aspect_ratio)) {
+  //     // if stimulus width is null, only use the image's natural width if the width value wasn't set
+  //     // in the if statement above, based on a specified height and maintain_aspect_ratio = true
+  //     width = img.naturalWidth;
+  //   }
+    
+  //   img.style.height = height.toString() + "px";
+  //   img.style.width = width.toString() + "px";
+  // }
+
+  constructor(private jsPsych: JsPsych) {}
+
+  trial(display_element: HTMLElement, trial: TrialType<Info>) {
+    let startTime = performance.now();
+    let words = [];
+    let buffer = '';
+    let wholebuffer = '';
+    let keypresses = '';
+    let responses = [];
+    let triggered = false;
+    let eventTime;
+
+    var canvas = this.renderImageOnCanvas(trial, display_element);
+
+    // Get the existing canvas and its parent element
+    let existingCanvas = display_element.querySelector('canvas');
+    let parentElement = existingCanvas.parentElement;
+
+    // Create a new canvas and get its context
+    let newCanvas = document.createElement('canvas');
+    let ctx = newCanvas.getContext('2d');
+
+    // Append the new canvas to the parent element of the existing canvas
+    parentElement.appendChild(newCanvas);
+
+    // Set the size of the new canvas
+    newCanvas.width = existingCanvas.width + 400; // Adjust as needed
+    newCanvas.height = 200; // Adjust as needed
+
+    // Center the new canvas
+    newCanvas.style.display = 'block';
+    newCanvas.style.margin = '0 auto';
+
+    // Set the font size and style for the text
+    ctx.font = '30px Arial'; // Adjust as needed
+    if (trial.visual_feedback == 'word') {
+      ctx.textAlign = 'center';
+    }
+    else if (trial.visual_feedback == 'whole') {
+      ctx.textAlign = 'left';
+    }
+    else if (trial.visual_feedback == 'long') {
+      ctx.textAlign = 'right';
+    }
+    ctx.textBaseline = 'middle';
 
     // function to end trial when it is time
     const end_trial = () => {
@@ -204,11 +327,14 @@ class ImageKeyboardResponsePlugin implements JsPsychPlugin<Info> {
         this.jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
       }
 
+      words.push(buffer);
+
       // gather the data to store for the trial
       var trial_data = {
-        rt: response.rt,
-        stimulus: trial.stimulus,
-        response: response.key,
+        responses,
+        words,
+        wholebuffer,
+        eventTime
       };
 
       // clear the display
@@ -222,16 +348,89 @@ class ImageKeyboardResponsePlugin implements JsPsychPlugin<Info> {
     var after_response = (info) => {
       // after a valid response, the stimulus will have the CSS class 'responded'
       // which can be used to provide visual feedback that a response was recorded
-      display_element.querySelector("#jspsych-image-keyboard-response-stimulus").className +=
-        " responded";
-
-      // only record the first response
-      if (response.key == null) {
-        response = info;
+      // Calculate the reaction time
+      let rt = performance.now() - startTime;
+      let key = info.key;
+      
+      if (key == 'enter') {
+        if (trial.trigger_end) {
+          if (triggered) {
+            ctx.fillStyle = 'green';
+            this.jsPsych.pluginAPI.setTimeout(() => {
+              end_trial();
+            }, trial.correction_delay);
+          }
+          else {
+            ctx.fillStyle = 'red';
+            this.jsPsych.pluginAPI.setTimeout(() => {
+              ctx.fillStyle = 'black';
+            }, trial.correction_delay);
+          }
+        }
+        else {
+          end_trial();
+        } 
       }
 
-      if (trial.response_ends_trial) {
-        end_trial();
+      // If the key is a backspace, remove the last character from the buffer
+      if (key == 'shift' || key == 'meta' || key == 'enter') {
+        // Do nothing
+      }
+      else if (key == '\\' || key == 'backspace') {
+        buffer = buffer.slice(0, -1);
+        wholebuffer = wholebuffer.slice(0, -1);
+      }
+      // If the key is a space, add the buffer to the words array and clear it
+      else if (key === ' ') {
+        if (trial.space_allowed) {
+          words.push(buffer);
+          buffer = "";
+          if (triggered) {
+            this.changeImageOnCanvas(trial, canvas, trial.stimulus_post);
+            eventTime = startTime - performance.now();
+          }
+        }
+        else { 
+          buffer += ' ';
+        }
+        wholebuffer += ' ';
+      }
+      // Otherwise, add the key to the buffer
+      else {
+        buffer += info.key;
+        wholebuffer += info.key;
+      }
+
+      // Store the key and reaction time
+      responses.push({
+        key: info.key,
+        rt: rt
+      });
+
+      if (trial.trigger_list.includes(buffer)) {
+        // If the buffer is a trigger, change the image
+        triggered = true;
+      }
+
+      if (trial.visual_feedback == 'word') {
+        // Update the typed text element with the current buffer
+        ctx.clearRect(0, 0, newCanvas.width, newCanvas.height);
+        // Update the canvas with the current buffer
+        ctx.fillText(buffer, newCanvas.width / 2, newCanvas.height / 2); // Adjust the coordinates as needed
+      }
+
+      if (trial.visual_feedback == 'whole') {
+        // Update the typed text element with the current buffer
+        ctx.clearRect(0, 0, newCanvas.width, newCanvas.height);
+        // Update the canvas with the current buffer
+        ctx.fillText(wholebuffer, 0, newCanvas.height / 2); // Adjust the coordinates as needed
+      }
+
+      if (trial.visual_feedback == 'long'){
+        // Update the typed text element with the current buffer
+        ctx.clearRect(0, 0, newCanvas.width, newCanvas.height);
+        // Update the canvas with the current buffer
+        ctx.fillText(wholebuffer, newCanvas.width, newCanvas.height / 2); // Adjust the coordinates as needed
       }
     };
 
@@ -240,8 +439,8 @@ class ImageKeyboardResponsePlugin implements JsPsychPlugin<Info> {
       var keyboardListener = this.jsPsych.pluginAPI.getKeyboardResponse({
         callback_function: after_response,
         valid_responses: trial.choices,
-        rt_method: "performance",
-        persist: false,
+        rt_method: 'performance',
+        persist: true,
         allow_held_key: false,
       });
     }
